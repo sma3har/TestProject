ddf650b8ac229adb2f38c6112a3f552fb7e41840
sma3har <sma3har@yahoo.com>
2021-06-24 15:23:05 -0500

    added sysout

--- src/main/java/com/example/demo/FirstSpringBootApplication.java
+++ src/main/java/com/example/demo/FirstSpringBootApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstSpringBootApplication {
 
     public static void main(String[] args) {
+        System.out.println("new comment");
         SpringApplication.run(FirstSpringBootApplication.class, args);
     }
 
